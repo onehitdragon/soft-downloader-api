89efa263614e2ac7e2e5f29ddff66fea0f6a1ca0
vinh <onehitdragon@gmail.com>
2022-12-26 10:10:15 +0700

    make api comment/to, comment/reply

--- src/main/java/com/example/softdownloaderapi/controller/CommentController.java
+++ src/main/java/com/example/softdownloaderapi/controller/CommentController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.validation.constraints.NotBlank;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.softdownloaderapi.model.*;
@@ -22,5 +25,29 @@ public class CommentController{
     @GetMapping("/getall/{idSoft}")
     public List<Comment> getCommentsBySoft(@PathVariable(value = "idSoft") @NotBlank Integer id){
         return commentRepository.getCommentsBySoft(id);
+    }
+
+    @PostMapping("/to/{idSoft}")
+    public ResponseEntity<ResponseMessage> commentTo(@PathVariable(value = "idSoft") @NotBlank Integer id, @NotBlank Integer commenterId, @NotBlank String content){
+        Comment comment = new Comment();
+        comment.setContent(content);
+        Comment result = commentRepository.insert(id, commenterId, comment);
+
+        return new ResponseEntity<ResponseMessage>(
+            new ResponseMessageWithOption<Comment>("success", "", result),
+            HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/replyto/{idSoft}")
+    public ResponseEntity<ResponseMessage> commentTo(@PathVariable(value = "idSoft") @NotBlank Integer id, @NotBlank Integer commenterId, @NotBlank String content, @NotBlank Integer receivedCommentId){
+        Comment comment = new Comment();
+        comment.setContent(content);
+        Comment result = commentRepository.insertReply(id, commenterId, comment, receivedCommentId);
+
+        return new ResponseEntity<ResponseMessage>(
+            new ResponseMessageWithOption<Comment>("success", "", result),
+            HttpStatus.OK
+        );
     }
 }
--- src/main/java/com/example/softdownloaderapi/repository/CommentRepository.java
+++ src/main/java/com/example/softdownloaderapi/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -51,6 +52,32 @@ public class CommentRepository {
             }
         }
 
+        session.close();
+
+        return result;
+    }
+
+    public Comment insert(int sortId, int commenterId, Comment comment){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String queryStr = "INSERT INTO Comments(content, commenterId, softwareId, createDate) VALUES ('"+ comment.getContent() +"', "+ commenterId +", "+ sortId +", CURRENT_TIMESTAMP)";
+        session.createNativeQuery(queryStr).executeUpdate();
+        transaction.commit();
+        queryStr = "SELECT * FROM comments WHERE id = (SELECT MAX(id) FROM comments)";
+        Comment insertedComment = session.createNativeQuery(queryStr, Comment.class).getSingleResult();
+        session.close();
+
+        return insertedComment;
+    }
+
+    public Comment insertReply(int sortId, int commenterId, Comment comment, int receivedCommentId){
+        Comment result = insert(sortId, commenterId, comment);
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String queryStr = "INSERT INTO ReplyComment(commentId, replyCommentId) VALUES ("+ receivedCommentId +", "+ result.getId() +")";
+        session.createNativeQuery(queryStr).executeUpdate();
+        transaction.commit();
         session.close();
 
         return result;
